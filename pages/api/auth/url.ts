b3b5d4f0c896a87761a255e31ba63d4da0004c12
Sam Starling <42478+samstarling@users.noreply.github.com>
2020-11-20 21:31:56 +0000

    Fix URL generation

--- pages/api/auth/url.ts
+++ pages/api/auth/url.ts
@@ -2,10 +2,11 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 import { installer } from "../../../util/installer";
 
-export default function (req: NextApiRequest, res: NextApiResponse) {
+export default async function (_: NextApiRequest, res: NextApiResponse) {
+  const result = await installer.generateInstallUrl({
+    scopes: ["app_mentions:read", "chat:write"],
+  });
   res.status(200).json({
-    url: installer.generateInstallUrl({
-      scopes: ["app_mentions:read", "chat:write"],
-    }),
+    url: result,
   });
 }
