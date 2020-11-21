408e6298ef265113de189f6f6c00ecd0dabf12a8
Sam Starling <42478+samstarling@users.noreply.github.com>
2020-11-21 09:34:23 +0000

    Fix config

--- config/config.js
+++ config/config.js
@@ -4,7 +4,10 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    database: process.env.DATABASE_URL,
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
+    dialectOptions: {
+      ssl: true,
+    },
   },
 };
