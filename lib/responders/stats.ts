d0d5de22d744ba5926f393b7f212f60b32047891
Sam Starling <42478+samstarling@users.noreply.github.com>
2020-11-21 19:40:34 +0000

    Fancier stats

--- lib/responders/stats.ts
+++ lib/responders/stats.ts
@@ -3,7 +3,32 @@ import { PrismaClient } from "@prisma/client";
 
 import { MentionEvent } from "../slack";
 import { Responder } from "./";
-import { GEN_ONE_POKEMON, emojiFor } from "../pokemon";
+import { GEN_ONE_POKEMON, emojiFor, pickOne, Pokemon } from "../pokemon";
+
+const statusFor = (pokemon: Pokemon): string => {
+  const name = pokemon.name.english;
+  return pickOne([
+    `${name} is doing OK, thanks for checking in.`,
+    `${name} is great – but a little hungry.`,
+    `${name} is annoyed that you forgot their birthday last week.`,
+    `${name} is good.`,
+    `${name} is alright.`,
+    `${name} is great.`,
+    `${name} has been better, actually.`,
+    `${name} has the sniffles.`,
+    `${name} is doing well.`,
+    `${name} is doing OK.`,
+    `${name} is just fine.`,
+    `${name} is having a rough day.`,
+    `${name} would like a hug.`,
+    `${name} could use.`,
+    `Looks like ${name} is happy.`,
+    `Your ${name} is looking healthy.`,
+    `Your ${name} is well.`,
+    `Your ${name} is just great.`,
+    `Your ${name} is swell.`,
+  ]);
+};
 
 export default {
   id: "query-stats",
@@ -44,18 +69,22 @@ export default {
         },
         {
           type: "section",
+          text: {
+            type: "mrkdwn",
+            text: statusFor(pokemon),
+          },
           fields: [
             {
               type: "mrkdwn",
               text: "*HP*",
             },
             {
-              type: "mrkdwn",
-              text: "*Attack*",
-            },
-            {
               type: "plain_text",
               text: `${pokemon.base.HP}`,
+            },
+            {
+              type: "mrkdwn",
+              text: "*Attack*",
             },
             {
               type: "plain_text",
@@ -66,12 +95,12 @@ export default {
               text: "*Defense*",
             },
             {
-              type: "mrkdwn",
-              text: "*Speed*",
-            },
-            {
               type: "plain_text",
               text: `${pokemon.base.Defense}`,
+            },
+            {
+              type: "mrkdwn",
+              text: "*Speed*",
             },
             {
               type: "plain_text",
