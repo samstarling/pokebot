1861c566b622c4511248ae9a875580886fb17c1f
Sam Starling <42478+samstarling@users.noreply.github.com>
2020-11-25 19:21:09 +0000

    API endpoint to import Pokemons

--- pages/api/import.ts
+++ pages/api/import.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Pokemon } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -8,12 +8,32 @@ import fs from "fs";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  const results: Array<{}> = [];
+  const results: Array<{
+    name: string;
+    pokedex_number: string;
+    generation: string;
+  }> = [];
 
   fs.createReadStream("./data/pokemon.csv")
     .pipe(csv())
     .on("data", (data) => results.push(data))
     .on("end", () => {
+      results.forEach(async (row) => {
+        await prisma.pokemon.upsert({
+          where: { number: parseInt(row.pokedex_number) },
+          create: {
+            name: row.name,
+            generation: parseInt(row.generation),
+            number: parseInt(row.pokedex_number),
+          },
+          update: {
+            name: row.name,
+            generation: parseInt(row.generation),
+            number: parseInt(row.pokedex_number),
+          },
+        });
+      });
+
       res.status(200).json({ results: results });
     });
 };
