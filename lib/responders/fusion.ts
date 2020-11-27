1a814f05c038dc9d82cebbae283f8234c14210bf
Sam Starling <42478+samstarling@users.noreply.github.com>
2020-11-27 19:27:52 +0000

    use Markdown

--- lib/responders/fusion.ts
+++ lib/responders/fusion.ts
@@ -32,7 +32,7 @@ export default {
         {
           type: "section",
           text: {
-            type: "plain_text",
+            type: "mrkdwn",
             text,
           },
         },
