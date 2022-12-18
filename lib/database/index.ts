9dca0dc275341f912a8636ff1c7247108bbcde40
Sam Starling <sam@incident.io>
2022-12-18 12:26:03 +0000

    Add database port

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -8,6 +8,7 @@ const db = new DataSource({
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT,
   entities: [Pokemon, Roll, Installation],
   schema: "public",
   synchronize: false,
--- scripts/import.ts
+++ scripts/import.ts
@@ -34,6 +34,7 @@ async function importPokes() {
       username: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
+      port: process.env.DATABASE_PORT,
       entities: [Pokemon, Roll, Installation],
       schema: "public",
       synchronize,
