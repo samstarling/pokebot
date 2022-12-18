902bb2bb9a24b4ce21ad4df8a1d9833b878bf010
Sam Starling <sam@incident.io>
2022-12-18 12:29:36 +0000

    Parse an int

--- scripts/import.ts
+++ scripts/import.ts
@@ -34,7 +34,7 @@ async function importPokes() {
       username: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
-      port: process.env.DATABASE_PORT,
+      port: parseInt(process.env.DATABASE_PORT),
       entities: [Pokemon, Roll, Installation],
       schema: "public",
       synchronize,
